01cb3b5397e113522c9782b1d8b10f99ea027586
Kondratiev, Vitaly (UK - London) <vkondratiev@deloitte.co.uk>
2014-09-29 23:11:10 +0100

    simple test added

--- test/node-todoist-sync_test.js
+++ test/node-todoist-sync_test.js
@@ -4,7 +4,13 @@ var assert = require('assert'),
   nodeTodoistSync = require('../lib/node-todoist-sync.js');
 
 describe('node-todoist-sync node module.', function() {
-  it('must be awesome', function() {
-    assert( nodeTodoistSync.awesome(), 'awesome');
+  it('can be instanciated', function() {
+  	var client = new nodeTodoistSync({
+  		email: 'test@user.com',
+  		password: 'myStrongPassword'
+  	});
+    assert.equal( client.email, 'test@user.com');
+    assert.equal( client.password, 'myStrongPassword');
+    assert.equal( client.isLoggedIn, false);
   });
 });
